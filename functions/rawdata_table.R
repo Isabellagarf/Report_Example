a1c1ecd1e0e4cd8703ca9d652e6c21a6b7f50bc5
Isabella Garfield <isabellagarfield@gmail.com>
2025-01-16 13:01:59 -0500

    testing tables
    Testing embedded table

--- functions/rawdata_table.R
+++ functions/rawdata_table.R
@@ -1,12 +1,12 @@
-rawdata_table <- function(title, id, years=1990:2015){
+rawdata_table <- function(title, id){
   require(gt)
   filename <- here::here("data", paste0(id, ".csv"))
   dat <- utils::read.csv(filename, stringsAsFactors = FALSE)
-  cols <- c("YEAR", "NUMBER_OF_SPAWNERS", "COMMON_POPULATION_NAME")
+ # cols <- c("Site", "N Vessels", "Duration Range (minutes)", "Duration Median (Minutes)")
   ishtml <- knitr::is_html_output()
   ispdf <- knitr::is_latex_output()
   isword <- !ishtml & !ispdf
   
-  dat <- dat[match(years, dat$YEAR),cols]
-  gt::gt(dat, groupname_col = "COMMON_POPULATION_NAME")
+  dat <- "Example_Tbl3_AMPreport.csv"
+  #gt::gt(dat, groupname_col = "COMMON_POPULATION_NAME")
 }
